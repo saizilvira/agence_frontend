14b0813a67ebea0539853f6ec940ad8415ffad88
luis.saiz1 <luis.saiz@uao.edu.co>
2023-05-02 16:39:06 -0500

    deploy

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://agencebackend-production.up.railway.app',
     headers: {
         'Content-Type': 'application/json',
     }
